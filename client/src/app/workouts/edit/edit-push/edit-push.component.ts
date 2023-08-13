40b301d531549ada558edc8d9ff6c8385f38d687
nikolovcdxx <nikolovcdxx@gmail.com>
2023-08-13 14:35:40 +0300

    Edit Legs Component

--- client/src/app/workouts/edit/edit-push/edit-push.component.ts
+++ client/src/app/workouts/edit/edit-push/edit-push.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./edit-push.component.css']
 })
 export class EditPushComponent {
-  @Input() exercises = {}
   
+
   onClickVideoHandler() {
     //
   }
